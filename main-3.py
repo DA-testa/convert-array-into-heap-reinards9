42f694df0f407e5577c0d9b7f60e340dd95d1792
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-03-19 22:01:30 +0200

    Update main-3.py

--- main-3.py
+++ main-3.py
@@ -1,32 +1,32 @@
-def process_jobs():
-    # Allocate jobs to threads like a Tetris game
-    thread_times = [0] * n_threads
-    job_start_times = []
+import sys
 
-    for job_duration in jobs:
-        # Find the thread with the least amount of work
-        thread_index = thread_times.index(min(thread_times))
+def build_heap(heap, n):
+    swaps = []
+    for i in range(n // 2, -1, -1):
+        sift_down(heap, swaps, i)
+    return swaps
 
-        # Record when the job will start
-        job_start_times.append((thread_index, thread_times[thread_index]))
+def sift_down(heap, swaps, i):
+    min_idx = i
+    l = 2 * i + 1
+    if l < len(heap) and heap[l] < heap[min_idx]:
+        min_idx = l
+    r = 2 * i + 2
+    if r < len(heap) and heap[r] < heap[min_idx]:
+        min_idx = r
+    if i != min_idx:
+        heap[i], heap[min_idx] = heap[min_idx], heap[i]
+        swaps.append((i, min_idx))
+        sift_down(heap, swaps, min_idx)
 
-        # Add the job duration to the thread's processing time
-        thread_times[thread_index] += job_duration
-
-    assert n_jobs == len(job_start_times)
-
-    # Print the thread numbers and job start times
-    for thread_num, starts_at in job_start_times:
-        print(f"Thread {thread_num} starts job at {starts_at} seconds")
-
-# Get input from user
-num_threads, num_jobs = input("Enter number of threads and jobs: ").split()
-n_threads = int(num_threads)
-n_jobs = int(num_jobs)
-jobs = list(map(int, input("Enter job durations: ").split()))
-
-# Process jobs and print output
-process_jobs()
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    data = list(map(int, sys.stdin.readline().split()))
+    assert n == len(data)
+    swaps = build_heap(data, n)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
 
 
 
