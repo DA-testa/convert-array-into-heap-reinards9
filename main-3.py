50e746faf31226b65a434382927f25308b7d8762
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-03-19 21:39:30 +0200

    Update main-3.py

--- main-3.py
+++ main-3.py
@@ -1,42 +1,22 @@
-def heapify (arr, n, swaps):
-  
-    for o in range(n//2, -1, -1):
-      
-        u = o
-        while t > u*2+1 :
-          
-            r = u*2+1
-          
-            if r + 1 < n and arr[r +1] < arr[r]:
-                r += 1
-              
-            if arr[u] > arr[r]:
-              
-                arr[u], arr[r ] = arr[r], arr[u]
-                swaps.append((u, r))
-              
-                u = r
-            else:
-              
-              break
+def process():
+  # in a nutshell, kods darbojas tā kā tetris
+  threads = [0] * n_threads
+  output = []
+  for job_duration in jobs:
+    # atrod piemērotāko threadu (ar vismazāko apstrādes ilgumu)
+    thread_i = threads.index(min(threads))
+    # saglabājam, kad darbs tiks sākts
+    output.append((thread_i, threads[thread_i]))
+    # pieskaita threadam darba ilgumu
+    threads[thread_i] += job_duration
 
-def build_heap (arr) :
-    t = len (arr)
-    swaps = []
-    heapify (arr, t, swaps)
-    return swaps
+  assert n_jobs == len(output)
 
-t = int (input() )
+  print("\n".join([f"{thread_num} {starts_at}" for thread_num, starts_at in output]))
 
-arr = list(map(int, input().split()))
+threads_and_jobs = input().split(" ")
+n_threads = int(threads_and_jobs[0])
+n_jobs = int(threads_and_jobs[1])
+jobs = [int(job) for job in input().split(" ")]
 
-swaps = build_heap(arr  )
-
-j = len(swaps)
-
-print(j)
-
-for o, u in swaps:
-
-  
-    print(o, u)
+process()
