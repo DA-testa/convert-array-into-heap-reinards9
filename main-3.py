3703ee1ed4e5a48982561d4fff10f0374cc89166
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-03-19 21:55:40 +0200

    Update main-3.py

--- main-3.py
+++ main-3.py
@@ -1,29 +1,28 @@
 def process():
-  
-  threads = [0] * threadss
-  output = []
-  for laiks in darbii:
-   
-    threadP = threads.index(min (threads) )
+    threads = [0] * n_threads
+    output = []
     
-    output.append(( threadP, threads[threadP] ) )
-    
-    threads[threadP ] += laiks
+    for job_duration in jobs:
+        # Find the thread with the shortest processing time
+        thread_i = threads.index(min(threads))
+        
+        # Save when the job will start
+        output.append((thread_i, threads[thread_i]))
+        
+        # Add the job duration to the thread's processing time
+        threads[thread_i] += job_duration
 
-  assert darbi == len(output   )
+    assert n_jobs == len(output)
 
-  print("\n".join( [f"{thread_num} {starts_at}" for thread_num, starts_at in output] ) )
+    print("\n".join([f"{thread_num} {starts_at}" for thread_num, starts_at in output]))
 
-threadsJ = input().split(" "  )
+try:
+    threads_and_jobs = input().strip().split(" ")
+    n_threads = int(threads_and_jobs[0])
+    n_jobs = int(threads_and_jobs[1])
+    jobs = [int(job) for job in input().strip().split(" ")]
+    process()
+except (ValueError, IndexError, AssertionError):
+    print("Invalid input format")
 
-
-threadss = int(threadsJ[0] )
-
-
-darbi = int(threadsJ[1])    
-
-
-darbii = [ int(darbs) for darbs in input().split(" ") ]
-
-process()
 
